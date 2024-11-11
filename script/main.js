366d22c26814e05c90ac411ea9d557468f5a1537
djtjsdud <djwnsdn@gmail.com>
2024-11-11 23:00:18 +0900

    feat : 서울구석구석 웹디자인 기능사 연습

--- script/main.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $(".popup-button").on("click", function(){
-        $("#popup-box").addClass("show");
-    });
-
-    $(".close-btn").on("click", function(){
-        $("#popup-box").removeClass("show");
-    });
-});
